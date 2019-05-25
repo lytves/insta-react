c0bd1a95775737c57f48becb6f204998eda8c7f8
lytves <22356770+lytves@users.noreply.github.com>
2019-05-25 23:47:49 +0200

    created components Feed y Posts

---
+++ src/components/Posts.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+export default class Posts extends Component {
+
+    render() {
+        return (
+
+            <div className="left">
+
+
+            </div>
+
+        )
+    }
+}
