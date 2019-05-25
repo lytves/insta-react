dc13181a242afe738e2f9c9d041950dfa887c050
lytves <22356770+lytves@users.noreply.github.com>
2019-05-25 23:21:36 +0200

    component Header - menu links added

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,6 +12,24 @@ export default class Header extends Component {
                     <a href="#" className="logo">
                         <img alt="logo" src={logo}></img>
                     </a>
+
+                    <nav className="links">
+                        <ul>
+                            <li>
+                                <a className="menu__links">
+                                    Tape
+                                </a>
+                            </li>
+
+                            <li>
+                                <a className="menu__links">
+                                    Account
+                                </a>
+                            </li>
+                        </ul>
+
+                    </nav>
+
                 </div>
 
 
