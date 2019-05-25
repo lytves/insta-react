1a09f3fde7fd70b57fe8218d4f3c835ea0c72ebe
lytves <22356770+lytves@users.noreply.github.com>
2019-05-26 00:52:31 +0200

    implemented JS condition to css style of rendering User

--- src/components/User.js
+++ src/components/User.js
@@ -6,7 +6,7 @@ export default class User extends Component {
 
         return (
 
-            <a href="#" className="user">
+            <a href="#" className={this.props.min ? "user min": "user"}>
                 <img src={this.props.src} alt={this.props.alt}/>
                 <div>{this.props.name}</div>
             </a>
