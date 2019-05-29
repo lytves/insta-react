6033f8e59c30be4378b5de9824e3bb37137f8f1d
lytves <22356770+lytves@users.noreply.github.com>
2019-05-29 19:33:04 +0200

    implementing Posts with usage of insta-service, that get a data in .json from json-server && was used lifecycle hook componentDidMount()

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,31 +1,94 @@
 import React, {Component} from 'react';
-import Post from './Post';
 import InstaService from '../services/insta-service'
+import User from "./User";
+import ErrorMessage from "./ErrorMessage";
 
 export default class Posts extends Component {
 
-    // Class field declarations syntaxis: InstaService, state
+    // Class field declarations: InstaService, state
 
     InstaService = new InstaService();
 
     state = {
 
-        posts:[],
-        error: false,
+        posts: [],
+        error: false
     };
 
+    componentDidMount() {
+        this.updatePosts();
+    }
+
+    updatePosts() {
+        this.InstaService.getAllPosts()
+            .then(this.onPostsLoaded)
+            .catch(this.onError)
+    }
+
+    onPostsLoaded = (posts) => {
+        // will mutate the state
+        this.setState({
+            // standard syntaxis
+            // posts: posts,
+            // new syntaxis
+            posts,
+            error: false
+        })
+    };
+
+    onError = (err) => {
+        this.setState({
+            error: true
+        })
+    };
+
+    renderItems(arr) {
+
+        return arr.map(item => {
+            const {name, altname, photo, src, alt, descr, id} = item;
+
+            return (
+
+                <div key={id} className="post">
+
+                    <User
+                        src={photo}
+                        alt={altname}
+                        name={name}
+                        min/>
+
+                    <img src={src} alt={alt}/>
+
+                    <div className="post__name">
+                        {name}
+                    </div>
+
+                    <div className="post__descr">
+                        {descr}
+                    </div>
+
+                </div>
+
+            )
+
+        });
+    }
+
     render() {
+
+        const {error, posts} = this.state;
+
+        if (error) {
+            return <ErrorMessage/>
+        }
+
+        const items = this.renderItems(posts);
+
         return (
 
             <div className="left">
 
-                <Post
-                    src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                    alt="nature"/>
-
-                <Post
-                    src="https://therightsofnature.org/wp-content/uploads/2018/01/turkey-3048299_1920.jpg"
-                    alt="nature"/>
+                {items}
 
             </div>
 
